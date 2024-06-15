a2b6b201ebd6598e9f713ccbf8095ad4ac317d71
unknown <ismayiloff522@gmail.com>
2024-06-15 13:24:55 +0400

    filter brand

--- public/js/app.js
+++ public/js/app.js
@@ -4,12 +4,13 @@ let allProducts;
 let filteredProducts;
 
 const getProduct = async () => {
-  await fetch('https://fakestoreapi.com/products')
+  await fetch('https://mocki.io/v1/c74f248a-3e51-4c88-aaf4-fdc759f1c451')
     .then(res => res.json())
     .then(data => {
       allProducts = data;
       displayProducts(allProducts);
-    });
+    })
+    .catch(error => console.error("Error fetching products from server ", error))
 }
 
 const displayProducts = (productsToDisplay) => {
@@ -21,6 +22,7 @@ const displayProducts = (productsToDisplay) => {
             <img src="${item.image}" alt="${item.title}" class="mx-auto h-60 object-contain mb-4">
             <h2 class="text-xl font-semibold mb-2">${item.title.slice(0, 20)}...</h2>
             <p class="text-gray-600 mb-4">${item.category}</p>
+            <p class="text-gray-600 mb-4">${item.brand}</p>
             <p class="text-lg font-bold mb-4">$${item.price}</p>
             <button class="bg-blue-500 text-white px-4 py-2 rounded-lg">Add to Cart</button>    
           `;
@@ -37,11 +39,26 @@ const filterCategory = (category) => {
   displayProducts(filteredProducts);
 }
 
+const filterBrand = (brand) => {
+  if (!brand) {
+    filteredProducts = allProducts;
+  } else {
+    filteredProducts = allProducts.filter(item => item.brand === brand)
+  }
+  displayProducts(filteredProducts)
+}
+
 searchInput.addEventListener('input', () => {
   const searchQuery = searchInput.value.toLowerCase();
-  const searchedProducts = filteredProducts.filter(item =>
-    item.title.toLowerCase().includes(searchQuery)
-  );
+  if (filteredProducts) {
+    var searchedProducts = filteredProducts.filter(item =>
+      item.title.toLowerCase().includes(searchQuery)
+    );
+  } else {
+    var searchedProducts = allProducts.filter(item =>
+      item.title.toLowerCase().includes(searchQuery)
+    );
+  }
   displayProducts(searchedProducts);
 });
 
